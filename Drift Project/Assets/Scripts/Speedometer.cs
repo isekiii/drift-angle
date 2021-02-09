9b0d869f355190a645d68ebc4c00196e15781b8d
Daiva-T <78407423+Daiva-T@users.noreply.github.com>
2021-02-09 12:16:06 +0200

    updated terrain layer

---
+++ Drift Project/Assets/Scripts/Speedometer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+     public GameObject needle;
+
+     private float startPosition = 220f, endPosition = -41;
+
+     private float desiredPosition;
+
+     public float vehicleSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void updateNeedle()
+    {
+        desiredPosition = startPosition - endPosition;
+        float temp = vehicleSpeed / 180;
+        needle.transform.eulerAngles = new Vector3(0, 0, (startPosition - temp * desiredPosition));
+    }
+}
