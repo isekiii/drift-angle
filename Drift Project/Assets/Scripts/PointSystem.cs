a6b32efa34053393a9b4eb9a66ac4c6be48a3d3b
Benas <steponaviciusbenas@gmail.com>
2021-02-17 15:23:52 +0200

    Point system added

---
+++ Drift Project/Assets/Scripts/PointSystem.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class PointSystem : MonoBehaviour
+{
+
+
+    public Rigidbody car;
+
+    public float currentScore;
+    public float totalScore;
+
+    public bool isDrifting = false;
+
+    public float driftAngle;
+
+    public Speedometer speedometer;
+
+    public TMP_Text currentText, totalText;
+
+
+    void Update()
+    {
+       driftAngle = Vector3.Angle(car.velocity, car.transform.forward);
+       
+        isDrifting = driftAngle > 15;
+        
+        UpdateScore();
+        UpdateText();
+    }
+
+    void UpdateScore()
+    {
+        if (isDrifting)
+        {
+            currentScore += driftAngle * speedometer.vehicleSpeed;
+        }
+        else
+        {
+            totalScore += currentScore;
+            currentScore = 0;
+        }
+    }
+
+    void UpdateText()
+    {
+        if (currentScore > 0)
+        {
+            currentText.text = currentScore.ToString();
+        }
+        else currentText.text = "";
+
+        totalText.text ="Total score: " +  totalScore.ToString();
+    }
+}
