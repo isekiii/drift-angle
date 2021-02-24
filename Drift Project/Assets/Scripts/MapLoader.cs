2a9e56dd5bfc5b0e56805a11788874109c6d95cf
tadeusas20 <78482124+tadeusas20@users.noreply.github.com>
2021-02-24 14:15:08 +0200

    Added map loading bar

---
+++ Drift Project/Assets/Scripts/MapLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MapLoader : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+    
+    public void LoadMap (int mapIndex)
+    {
+        StartCoroutine(LoadAsynchronously(mapIndex));
+    }
+
+    IEnumerator LoadAsynchronously (int mapIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(mapIndex);
+
+        loadingScreen.SetActive(true);
+
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+            //Debug.Log(progress);
+
+            yield return null;
+        }
+    }
+}
