27fd04a3bec2083199fef305b4c9c1ea1c4f5603
Benas <steponaviciusbenas@gmail.com>
2021-02-26 14:04:00 +0200

    Audio Settings fixed

--- Drift Project/Assets/Scripts/SettingsMenu.cs
+++ Drift Project/Assets/Scripts/SettingsMenu.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    
 
     public Dropdown resolutionDropDown;
 
@@ -47,6 +48,7 @@ public class SettingsMenu : MonoBehaviour
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("Volume", volume);
+       
     }
 
     public void SetQuality(int qualityIndex)
