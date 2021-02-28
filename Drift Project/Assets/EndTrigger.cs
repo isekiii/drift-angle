65dec14e2e4b8fdecaed5baec6f28db67569c88a
Benas <steponaviciusbenas@gmail.com>
2021-02-28 16:18:46 +0200

    Finish trigger added to map1

---
+++ Drift Project/Assets/EndTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndTrigger : MonoBehaviour
+{
+    private int count = 0;
+    [SerializeField] private TMP_Text endText;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(count);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (count>0) EndGame();
+            count++;
+            
+            
+        }
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    void EndGame()
+    {
+        Time.timeScale = 0.1f;
+        endText.text = "!!!FINISH!!!";
+
+    }
+}
