e4e1b6c5c3328d2d719986974540e8d4efdd06f9
tadeusas20 <78482124+tadeusas20@users.noreply.github.com>
2021-03-09 13:22:30 +0200

    Car selection

---
+++ Drift Project/Assets/Scripts/CarModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarModel : MonoBehaviour
+{
+    [SerializeField] private GameObject[] carModels;
+
+    private void Awake()
+    {
+        ChooseCarModel(SaveManager.instance.currentCar);
+    }
+
+    private void ChooseCarModel(int index)
+    {
+        Instantiate(carModels[index], transform.position, Quaternion.identity, transform);
+    }
+}
---
+++ Drift Project/Assets/Scripts/CarSelection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarSelection : MonoBehaviour
+{
+    //private int currentCar;
+    public int index;
+
+    private void Start()
+    {
+        SaveManager.instance.currentCar = index;
+        //currentCar = SaveManager.instance.currentCar;
+        //SelectCar(currentCar);
+    }
+
+    //private void SelectCar (int index)
+    //{
+    //    //previousButton.interactable = (index != 0);
+    //    //nextButton.interactable = (index != transform.childCount-1);
+
+    //    for (int i=0; i < transform.childCount; i++)
+    //    {
+    //        transform.GetChild(i).gameObject.SetActive(i == index);
+    //    }
+    //}
+
+    //public void ChangeCar (int change)
+    //{
+    //    currentCar += change;
+
+    //    if (currentCar > transform.childCount - 1)
+    //        currentCar = 0;
+    //    else if (currentCar < 0)
+    //        currentCar = transform.childCount - 1;
+
+    //    SaveManager.instance.currentCar = currentCar;
+    //    SaveManager.instance.Save();
+
+    //    SelectCar(currentCar);
+    //}
+}
